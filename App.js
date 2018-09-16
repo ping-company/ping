e27034c3215a856085b9ef38d1d7bbc0e775e1d2
Dillion Verma <dillionverma@hotmail.com>
2018-09-16 02:32:55 -0400

    Firebase setup (#1)
    * Issue firebase-perf.zze not found
    
    * First firebase working version

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import firebase from 'react-native-firebase';
+import type { Notification, NotificationOpen } from 'react-native-firebase';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,6 +20,84 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+
+  componentWillMount() {
+
+    var config = {
+    apiKey: "AIzaSyAJZnkc5Nd7-DIGuLkmqYBXG6_yslLL8Lc",
+    authDomain: "ping-fb.firebaseapp.com",
+    databaseURL: "https://ping-fb.firebaseio.com",
+    storageBucket: "ping-fb.appspot.com",
+    appId:"1:26781916683:android:6e1b08e5872806e6",
+    projectId: "ping-fb",
+    messagingSenderId: "26781916683"
+  };
+        firebase.initializeApp(config);
+
+    }
+
+  async componentDidMount() {
+        const notificationOpen: NotificationOpen = await firebase.notifications().getInitialNotification();
+        if (notificationOpen) {
+            const action = notificationOpen.action;
+            const notification: Notification = notificationOpen.notification;
+            var seen = [];
+            alert(JSON.stringify(notification.data, function(key, val) {
+                if (val != null && typeof val == "object") {
+                    if (seen.indexOf(val) >= 0) {
+                        return;
+                    }
+                    seen.push(val);
+                }
+                return val;
+            }));
+        } 
+        const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.Max)
+                .setDescription('My apps test channel');
+
+// Create the channel
+        firebase.notifications().android.createChannel(channel);
+        this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
+            // Process your notification as required
+            // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+        });
+        this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
+            // Process your notification as required
+            notification
+                .android.setChannelId('test-channel')
+                .android.setSmallIcon('ic_launcher');
+            firebase.notifications()
+                .displayNotification(notification);
+            
+        });
+        this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
+            // Get the action triggered by the notification being opened
+            const action = notificationOpen.action;
+            // Get information about the notification that was opened
+            const notification: Notification = notificationOpen.notification;
+            var seen = [];
+            alert(JSON.stringify(notification.data, function(key, val) {
+                if (val != null && typeof val == "object") {
+                    if (seen.indexOf(val) >= 0) {
+                        return;
+                    }
+                    seen.push(val);
+                }
+                return val;
+            }));
+            firebase.notifications().removeDeliveredNotification(notification.notificationId);
+            
+        });
+    }
+
+    componentWillUnmount() {
+        this.notificationDisplayedListener();
+        this.notificationListener();
+        this.notificationOpenedListener();
+    }
+
+
   render() {
     return (
       <View style={styles.container}>
